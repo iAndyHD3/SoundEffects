c2d61c3219790a1bfd154ab44f78eaedebf20c95
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2022-12-03 18:26:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SoundEffects
+A geometry dash mod that adds sound effects to gameplay events
+warning: buggy and incomplete
+
+## Sounds
+
+- `orbJump01.ogg` is played when activating an orb
+- `jump01.ogg` played when you jump (bad implemented and very buggy at the moment)
+
+## Setup
+
+clone and configure/build with cmake using x86 msvc
+
+### Manual Setup
+if u want to do it in cmd then
+```bash
+# Configure
+cmake -B build -A win32
+# Build
+cmake --build build --config Release
+```
---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <matdash.hpp>
+
+// defines add_hook to use minhook
+#include <matdash/minhook.hpp>
+
+// lets you use mod_main
+#include <matdash/boilerplate.hpp>
+
+// matdash::create_console
+#include <matdash/console.hpp>
+
+// gd.h includes cocos2d.h
+#include <gd.h>
+#include <fmt/format.h>
+using namespace gd;
+
+
+#define playSound(str) gd::GameSoundManager::sharedState()->playSound(str)
+#define MEMBERBYOFFSET(type, class, offset) *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(class) + offset)
+#define MBO MEMBERBYOFFSET
+
+/*
+to debug sounds existing .ogg files can be used e.g
+gold01.ogg, gold02.ogg, quitSound_01.ogg
+however std::cout is better than randomly playing sounds lol
+*/
+
+void PlayerObject_ringJump(void* self, GameObject* ring) {
+    matdash::orig<&PlayerObject_ringJump>(self, ring);
+	if(ring->m_bHasBeenActivated) playSound("orbJump01.ogg");
+}
+
+void PlayLayer_update_(gd::PlayLayer* self, float dt) {
+	
+	auto p1 = self->m_pPlayer1;
+	
+	bool isFalling = p1->playerIsFalling();
+	bool isPressingDown = MBO(int, p1, 0x611) != 0;
+	bool onGround = !p1->m_isOnGround && (int)p1->unk610 == 213;
+	
+	matdash::orig<&PlayLayer_update_, matdash::Thiscall>(self, dt);
+
+	//idk this is crazy (and stupid)
+	if(!isFalling && !p1->playerIsFalling() && isPressingDown && onGround){
+		//std::cout << "jumped" << std::endl;
+		playSound("jump01.ogg");
+	}
+	//std::cout << fmt::format("isFalling: {}, isPressingDown: {}, onGround: {}, p1->playerIsFalling: {}", isFalling, isPressingDown, onGround, p1->playerIsFalling()) << std::endl;
+}
+
+
+
+void mod_main(HMODULE) {
+
+	//make sure to comment this for release
+	//matdash::create_console();
+  
+    matdash::add_hook<&PlayerObject_ringJump>(gd::base + 0x1F4FF0);
+    matdash::add_hook<&PlayLayer_update_, matdash::Thiscall>(gd::base + 0x2029c0);
+}
